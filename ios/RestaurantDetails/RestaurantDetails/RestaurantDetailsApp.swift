7063aab0a649161c2f87fe7099cf35751c95ae93
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2025-10-24 14:47:21 +0800

    - Add iOS folder - Add Xcode project to iOS folder

---
+++ ios/RestaurantDetails/RestaurantDetails/RestaurantDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantDetailsApp.swift
+//  RestaurantDetails
+//
+//  Created by Kelvin Ricafort on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
