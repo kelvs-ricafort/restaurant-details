73d2a8c15d00ea114b10fa8036c0686cc8eb61d4
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2025-10-24 21:30:16 +0800

    - Add RestaurantViewModel

---
+++ android/app/src/main/java/com/kelvsricafort101/wordpress/restaurantdetails/viewmodels/RestaurantViewModel.kt
@@ -0,0 +1,17 @@
+package com.kelvsricafort101.wordpress.restaurantdetails.viewmodels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.kelvsricafort101.wordpress.restaurantdetails.data.mock.MockData
+import com.kelvsricafort101.wordpress.restaurantdetails.data.models.MenuItem
+
+class RestaurantViewModel: ViewModel() {
+    val restaurant = mutableStateOf(MockData.sampleRestaurants)
+    val cartItems = mutableStateOf<List<MenuItem>>(emptyList())
+
+    fun addToCart(item: MenuItem) {
+        cartItems.value = cartItems.value + item
+    }
+
+    fun getCartCount(): Int = cartItems.value.size
+}
