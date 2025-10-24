8de817deb63c1f0b7e4f9e79179513b97f5aaec2
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2025-10-24 21:24:05 +0800

    - Add new color palette - Add new typography - Rename RestaurantDetailsTheme to RestaurantAppTheme

--- android/app/src/main/java/com/kelvsricafort101/wordpress/restaurantdetails/ui/theme/Color.kt
+++ android/app/src/main/java/com/kelvsricafort101/wordpress/restaurantdetails/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.kelvsricafort101.wordpress.restaurantdetails.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val OliveGreen = Color(0xFF708238)
+val LightOlive = Color(0xFFAFC97E)
+val CreamWhite = Color(0xFFFFFBE6)
+val DarkBrown = Color(0xFF3E2723)
